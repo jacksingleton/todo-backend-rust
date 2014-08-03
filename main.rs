28d2789e1e5f114c82bdf49c33271da3e4b41c7a
Jack Singleton <git@jacksingleton.com>
2014-08-02 22:00:41 -0400

    add test for find in addition to create and implement find and create. id creation is still todo

--- main.rs
+++ main.rs
@@ -1,25 +1,25 @@
 use std::collections::HashMap;
 
+//#[test]
+//#[should_fail]
+//fn create_and_read_todo() {
+//    // Given
+//    let mut repo = TodoRepo::new();
+//    let todo = "I am a new todo";
+//
+//    // When
+//    let todo_id = { repo.create(todo) };
+//    let read_todo = repo.find(todo_id);
+//
+//    // Then
+//    assert_eq!(read_todo, &todo);
+//}
+
+
 #[test]
-#[should_fail]
-fn create_and_read_todo() {
+fn create_should_add_todo_to_store() {
     // Given
-    let repo = TodoRepo::new();
-    let todo = "I am a new todo";
-
-    // When
-    let todo_id = repo.create(todo);
-    let read_todo = repo.find(todo_id);
-
-    // Then
-    assert_eq!(todo, read_todo);
-}
-
-
-#[test]
-fn create_should_add_todo_to_global() {
-    // Given
-    let repo = TodoRepo::new();
+    let mut repo = TodoRepo::new();
     let new_todo = "new todo";
 
     // When
@@ -29,27 +29,41 @@ fn create_should_add_todo_to_global() {
     assert_eq!(repo.store.len(), 1);
 }
 
+#[test]
+fn find_should_find_an_existing_todo() {
+    // Given
+    let mut repo = TodoRepo::new();
+    let todo_key = "todo-key";
+    let todo = "existing todo";
+
+    repo.store.insert(todo_key, todo);
+
+    // When
+    let found_todo = repo.find(todo_key);
+
+    // Then
+    assert_eq!(found_todo, &todo);
+}
+
 struct TodoRepo<'r> {
     store: HashMap<&'r str, &'r str>
 }
 
 impl <'r> TodoRepo<'r> {
-    //let mut map = HashMap::new();
-    //map.insert("some-id", new_todo);
-    //let retrieved_todo = map.find(&"some-id").unwrap();
-
     fn new<'r>() -> TodoRepo<'r> {
         TodoRepo {
             store: HashMap::new()
         }
     }
 
-    fn create(&self, todo: &str) -> &str {
-        "blah"
+    fn create(&mut self, todo: &'r str) -> &str {
+        let id = "some-id";
+        self.store.insert(id, todo);
+        id
     }
 
-    fn find(&self, id: &str) -> &str {
-        "blah"
+    fn find(&self, id: &'r str) -> &&'r str {
+        self.store.find(&id).unwrap()
     }
 }
 
