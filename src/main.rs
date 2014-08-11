4ad3604cfea7cbf1597351784e2981ba9c6dc44e
Jack Singleton <git@jacksingleton.com>
2014-08-10 23:59:58 -0700

    add hello world web server with iron, allow adding and reading a single todo

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,20 @@
-use std::collections::HashMap;
+extern crate http;
+extern crate iron;
 
-#[test]
-fn create_and_read_two_todos() {
-    // Given
-    let mut repo = TodoRepo::new();
-    let new_todo_1 = "new-todo-1";
-    //let new_todo_2 = "new-todo-2";
+use std::io::net::ip::Ipv4Addr;
+use iron::{Iron, Chain, Alloy, Request, Response, Server, Status, Unwind, FromFn};
 
-    // When
-    let todo_id_1 = repo.create(new_todo_1);
-    let read_todo_1 = repo.find(todo_id_1);
+use todo_repo::TodoRepo;
 
-    //let todo_id_2 = repo.create(new_todo_2);
-    //let read_todo_2 = repo.find(todo_id_2);
+mod todo_repo;
 
-    // Then
-    assert_eq!(read_todo_1, &new_todo_1);
-    //assert_eq!(read_todo_2, &new_todo_2);
+fn hello_world(_req: &mut Request, res: &mut Response, _alloy: &mut Alloy) -> Status {
+    let _ = res.serve(::http::status::Ok, "Hello, world!");
+    Unwind
 }
 
-#[test]
-fn create_should_add_todo_to_store() {
-    // Given
-    let mut repo = TodoRepo::new();
-    let new_todo = "new todo";
-
-    // When
-    repo.create(new_todo);
-
-    // Then
-    assert_eq!(repo.store.len(), 1);
+fn main() {
+    let mut server: Server = Iron::new();
+    server.chain.link(FromFn::new(hello_world));
+    server.listen(Ipv4Addr(127, 0, 0, 1), 3000);
 }
-
-#[test]
-fn find_should_find_an_existing_todo() {
-    // Given
-    let todo_key = "todo-key";
-    let todo = "existing todo";
-
-    let mut repo = TodoRepo::new();
-    repo.store.insert(todo_key, todo);
-
-    // When
-    let found_todo = repo.find(todo_key);
-
-    // Then
-    assert_eq!(found_todo, &todo);
-}
-
-
-struct TodoRepo<'r> {
-    store: HashMap<&'r str, &'r str>
-}
-
-impl <'r> TodoRepo<'r> {
-    fn new<'r>() -> TodoRepo<'r> {
-        TodoRepo {
-            store: HashMap::new()
-        }
-    }
-
-    fn create(&mut self, todo: &'r str) -> &'r str {
-        let id = "some-id";
-        self.store.insert(id, todo);
-        id
-    }
-
-    fn find(&'r self, id: &'r str) -> &'r &str {
-        self.store.find(&id).unwrap()
-    }
-}
-
---
+++ src/todo_repo.rs
@@ -0,0 +1,83 @@
+extern crate uuid;
+
+use std::collections::HashMap;
+use std::rand;
+
+use self::uuid::Uuid;
+
+#[test]
+fn create_and_read_multiple_todos() {
+    // Given
+    let mut repo = TodoRepo::new();
+    let new_todo_1 = "new-todo-1";
+    let new_todo_2 = "new-todo-2";
+
+    // When
+    let todo_id_1 = repo.create(new_todo_1);
+    let todo_id_2 = repo.create(new_todo_2);
+
+    let read_todo_1 = repo.find(todo_id_1);
+    let read_todo_2 = repo.find(todo_id_1);
+
+    // ??? Why does this not compile?
+    // error: cannot borrow `repo` as mutable because it is also borrowed as immutable
+    //repo.create("blah");
+
+    // Then
+    assert_eq!(read_todo_1, &new_todo_1);
+    assert_eq!(read_todo_2, &new_todo_2);
+}
+
+#[test]
+fn create_should_add_todo_to_store() {
+    // Given
+    let mut repo = TodoRepo::new();
+    let new_todo = "new todo";
+
+    // When
+    repo.create(new_todo);
+
+    // Then
+    assert_eq!(repo.store.len(), 1);
+}
+
+#[test]
+fn find_should_find_an_existing_todo() {
+    // Given
+    let todo_key = "todo-key";
+    let todo = "existing todo";
+
+    let mut repo = TodoRepo::new();
+    repo.store.insert(todo_key, todo);
+
+    // When
+    let found_todo = repo.find(todo_key);
+
+    // Then
+    assert_eq!(found_todo, &todo);
+}
+
+
+pub struct TodoRepo<'r> {
+    store: HashMap<&'r str, &'r str>
+}
+
+impl <'r> TodoRepo<'r> {
+    fn new<'r>() -> TodoRepo<'r> {
+        TodoRepo {
+            store: HashMap::new()
+        }
+    }
+
+    fn create(&mut self, todo: &'r str) -> &'r str {
+        //let id = Uuid::new_v4().to_string().as_slice();
+        let id = "dummy-id";
+        self.store.insert(id, todo);
+        id
+    }
+
+    fn find(&'r self, id: &'r str) -> &'r &str {
+        self.store.find(&id).unwrap()
+    }
+}
+
